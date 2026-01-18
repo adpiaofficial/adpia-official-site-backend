2b7946611b8aa3974de79b8934246ebe6440074a
이상화 <sanghwa1215@naver.com>
2026-01-19 02:16:28 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/org/adpia/official/AdpiaOfficialSiteApplication.java
@@ -0,0 +1,13 @@
+package org.adpia.official;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpiaOfficialSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdpiaOfficialSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/adpia/official/AdpiaOfficialSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adpia.official;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdpiaOfficialSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
