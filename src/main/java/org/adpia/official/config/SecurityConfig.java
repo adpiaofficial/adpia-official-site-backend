64b07cdad5f91ca79c2a779949af818b6cf566b1
이상화 <sanghwa1215@naver.com>
2026-02-04 15:11:57 +0900

    fix: 시큐리티 버그 픽스

--- src/main/java/org/adpia/official/config/SecurityConfig.java
+++ src/main/java/org/adpia/official/config/SecurityConfig.java
@@ -98,7 +98,12 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfig() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*"));
+		config.setAllowedOriginPatterns(List.of(
+			"http://localhost:*",
+			"http://127.0.0.1:*",
+			"https://adpia.or.kr",
+			"https://www.adpia.or.kr"
+		));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
